c82d998f0fb6972d97947b74e965bc6a61b2c31e
Phương Thảo <124881167+Phuongthao888@users.noreply.github.com>
2023-04-09 15:28:06 +0700

    Create TongDuong.c

---
+++ TongDuong.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#define max 50
+int nhapsonguyen(){
+	int n;
+	do{
+		printf("nhap phan tu n: ");
+		scanf("%d",&n);
+	}while(n<0 || n>max);
+return n;	
+}
+
+int nhapmang(int a[], int n){
+	for(int i=0; i<n ; i++)
+	{
+		printf("nhap phan tu a[%d]: ", i);
+		scanf("%d", &a[i]);
+		if(a[i]==0)
+			return 0;
+	}
+}
+
+int xuatmang(int a[], int n){
+	for(int i=0; i<n; i++)
+		printf("%4d", a[i]);
+}
+
+int TongDuong(int a[], int n){
+	if(n==0)
+	return 0;
+	if(a[n-1]>0)
+		return a[n-1] + TongDuong(a,n-1);
+		return TongDuong(a,n-1);
+}
+
+int main() {
+  	int n;
+  	int a[max];
+  	n = nhapsonguyen();
+  	
+  	nhapmang(a,n);
+  	printf("\n mang sau khi nhap la: \n");
+ 	xuatmang(a,n);
+  	
+  	int kq = TongDuong(a,n);
+  	printf("\nket qua tinh tong so duong trong mang: %d", kq);
+  return 0;
+}
