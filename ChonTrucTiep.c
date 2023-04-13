261724be337e913350b5713b16a14e6cc678eab5
Phương Thảo <124881167+Phuongthao888@users.noreply.github.com>
2023-04-13 11:15:01 +0700

    Update ChonTrucTiep.c

--- ChonTrucTiep.c
+++ ChonTrucTiep.c
@@ -1,51 +1,28 @@
 #include<stdio.h>
-#define max 50
-int nhapsonguyen(){
-	int n;
-	do{
-		printf("nhap phan tu n: ");
-		scanf("%d",&n);
-	}while(n<0 || n>max);
-return n;	
-}
 
-int nhapmang(int a[], int n){
-	for(int i=0; i<n ; i++)
-	{
-		printf("nhap phan tu a[%d]: ", i);
-		scanf("%d", &a[i]);
-		if(a[i]==0)
-			return 0;
+void xuatmang(int a[], int n){
+	for(int i=0; i<n; i++){
+		printf("%4d", a[i]);
 	}
 }
 
-int xuatmang(int a[], int n){
-	for(int i=0; i<n; i++)
-		printf("%4d", a[i]);
-}
 void chontructiep(int a[], int n){
-    for(int i=0; i<n-1; i++)
-    {
-        int min = i;
-        for (int j = i+1; j<n; j++){
-            if (a[j]<a[min]){ //tim ptu tiep theo nho hon
-                min = j;
-            }
-        }
-        int tam = a[i];
-        a[i] = a[min];
-        a[min] = tam;
-    }
+	for(int i=0; i<n; i++){
+		int min = i;
+		for(int j=i+1; j<n; j++){
+			if(a[j]< a[min])
+				min = j;
+		}
+		int tg = a[i];
+		a[i] = a[min];
+		a[min] = tg;
+	}
 }
 int main(){
-  	int n;
-  	int a[max];
-  	n = nhapsonguyen();
-  	
-  	nhapmang(a,n);
- 	xuatmang(a,n);
- 	 
-  	printf("\n sau khi sap xep chon truc tiep \n");
-  	chontructiep(a,n);
-  	xuatmang(a,n);
+	int n= 5;
+	int a[]= {44,10,5,1,2};
+	
+	chontructiep(a,n);
+	xuatmang(a,n);
+return 0;
 }
